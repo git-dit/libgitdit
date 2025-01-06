245568c38707442027c1a42d279cbf5cf3f9b931
Julian Ganz <neither@nut.email>
2025-01-06 14:18:32 +0100

    reference: add Reference trait
    Currently, `libgitdit` is based on `git2`.  We do want to support using
    other git implementations in the future, namely `gix`, and are thus
    currently introducing abstractions.
    
    References are quite essential for git-dit, as some vital information is
    encoded in them. Previously, all that was spread over `issue` and other
    modules. This change now introduces a trait with basic querying fns that
    we implement for `git2::Reference` and future alternatives.

--- src/reference.rs
+++ src/reference.rs
@@ -5,3 +5,51 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //! References and reference related utilities
+
+use std::borrow::Cow;
+use std::path::Path;
+
+/// A git reference
+pub trait Reference<'r> {
+    /// Type for reference names
+    type Name;
+
+    /// Type for holding [Path] represenations of references
+    type Path: AsRef<Path>;
+
+    /// Type used for representing Object IDs
+    type Oid;
+
+    /// Retrieve the name of the reference
+    fn name(&'r self) -> Self::Name;
+
+    /// Retrieve the [Path] representation of this reference
+    fn as_path(&'r self) -> Self::Path;
+
+    /// Retrieve the target of this reference
+    ///
+    /// This fn will return the target if this reference is direct. For indirect
+    /// references, this fn will return [None].
+    fn target(&'r self) -> Option<Self::Oid>;
+}
+
+impl<'r> Reference<'r> for git2::Reference<'_> {
+    type Name = Cow<'r, str>;
+    type Path = Cow<'r, Path>;
+    type Oid = git2::Oid;
+
+    fn name(&'r self) -> Self::Name {
+        String::from_utf8_lossy(self.name_bytes())
+    }
+
+    fn as_path(&'r self) -> Self::Path {
+        match Reference::name(self) {
+            Cow::Borrowed(p) => Cow::Borrowed(Path::new(p)),
+            Cow::Owned(p) => Cow::Owned(p.into()),
+        }
+    }
+
+    fn target(&'r self) -> Option<Self::Oid> {
+        self.target()
+    }
+}
