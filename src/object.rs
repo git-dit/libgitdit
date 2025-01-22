37a8127571bb26cc71f3c233f077ec92d947e17f
Julian Ganz <neither@nut.email>
2025-01-22 19:16:45 +0100

    object: add trait for object databases
    Currently, `libgitdit` is based on `git2`. We do want to support using
    other git implementations in the future, namely `gix`, and are thus
    currently introducing abstractions.
    
    We did not yet have the necessary git agnostic abstractions for
    retrieving and creating commits or trees. This change introduces a new
    trait that allows querying commits and trees.

--- src/object.rs
+++ src/object.rs
@@ -6,5 +6,38 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //! Object related facilities
 
+use crate::base::Base;
+use crate::error::{self, ResultExt};
+
 pub mod commit;
 pub mod tree;
+
+use self::commit::Commit;
+
+/// An object database
+pub trait Database<'r>: Base {
+    /// Type used for representing commits
+    type Commit: Commit<Oid = Self::Oid>;
+
+    /// Type used for representing trees
+    type Tree;
+
+    /// Retrieve a specific commit
+    fn find_commit(&'r self, oid: Self::Oid) -> error::Result<Self::Commit, Self::InnerError>;
+
+    /// Retrieve a specific tree
+    fn find_tree(&'r self, oid: Self::Oid) -> error::Result<Self::Tree, Self::InnerError>;
+}
+
+impl<'r> Database<'r> for git2::Repository {
+    type Commit = git2::Commit<'r>;
+    type Tree = git2::Tree<'r>;
+
+    fn find_commit(&'r self, oid: Self::Oid) -> error::Result<Self::Commit, Self::InnerError> {
+        git2::Repository::find_commit(self, oid).wrap_with_kind(error::Kind::CannotGetCommit)
+    }
+
+    fn find_tree(&'r self, oid: Self::Oid) -> error::Result<Self::Tree, Self::InnerError> {
+        git2::Repository::find_tree(self, oid).wrap_with_kind(error::Kind::CannotGetTree)
+    }
+}
