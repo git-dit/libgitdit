be54422f88dbb0b0c92b9479b6f684ccb156a869
Julian Ganz <neither@nut.email>
2025-02-10 20:45:36 +0100

    gc: remove unnecessary empty lines

--- src/gc.rs
+++ src/gc.rs
@@ -5,8 +5,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-//
-
 //! Garbage collecting utilities
 //!
 //! This module provides git-dit related garbage collection utilites.
@@ -289,4 +287,3 @@ mod tests {
         assert_eq!(refs_to_collect, collected);
     }
 }
-
